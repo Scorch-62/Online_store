57d47805f48621081ccb65bf38cf6f35e16aee00
Scorch-62 <ts3zar@yandex.ru>
2025-04-07 21:21:54 +1000

    Delete store/catalog directory

--- store/catalog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('catalog/', views.catalog, name='catalog'),
-    path('category/', views.category, name='category'),
-]
